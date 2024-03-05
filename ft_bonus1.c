8e31cf2fed7ea1cedabd8f26338bdf5fc97b9f02
Raja Pandiperumal <rpandipe@c2r4s5.42luxembourg.lu>
2024-03-05 09:43:49 +0100

    bonus1 done

--- ft_bonus1.c
+++ ft_bonus1.c
@@ -6,7 +6,7 @@
 /*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 18:38:05 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/03/04 15:10:36 by rpandipe         ###   ########.fr       */
+/*   Updated: 2024/03/05 09:37:33 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,31 @@ int	get_width(const char **format)
 		(*format)++;
 	}
 	return (width);
+}
+
+char	*padzeros(char *ans, int width, int len, char flag)
+{
+	int		offset;
+	char	*new;
+
+	offset = check_negative(ans);
+	if (offset == 1 && flag == '.')
+	{
+		width = width + 1;
+		if (len < width)
+		{
+			new = (char *)ft_calloc(width + 1, sizeof(char));
+			ft_strlcpy(new, ans, width + 1);
+			free(ans);
+			ans = new;
+		}
+	}
+	if (len < width)
+	{
+		ft_memmove(ans + offset + (width - len), ans + offset, len - offset);
+		ft_memset(ans + offset, '0', (size_t)(width - len));
+	}
+	return (ans);
 }
 
 void	ft_leftjust(va_list *ap, int *count, const char **format)
@@ -75,11 +100,7 @@ void	ft_zeropad(va_list *ap, int *count, const char **format, int flag)
 		if (flag == 1)
 			ft_memset(ans + len, '0', (size_t)(width - len));
 		else
-		{
-			offset = check_negative(ans);
-			ft_memmove(ans + offset + (width - len), ans + offset, len - offset);
-			ft_memset(ans + offset, '0', (size_t)(width - len));
-		}
+			ans = padzeros(ans, width, len, 'c');
 	}
 	ft_putstr_count(ans, count);
 	free(ans);
@@ -89,21 +110,26 @@ void	ft_precision(va_list *ap, int *count, const char **format)
 {
 	int		width;
 	char	c;
+	char	*p_ans;
 	char	*ans;
 	int		len;
 
 	width = get_width(format);
 	c = **format;
-	if (c == 'c' || c == 's' || c == 'p' || c == '%')
-		return ;
-	ans = b_format_sorter(c, ap, width, format);
+	p_ans = b_format_sorter(c, ap, width, format);
+	len = ft_strlen(p_ans);
 	if (c != **format)
 		return ;
-	if (len < width)
+	if (c == 's' && len > width)
 	{
-		ft_memmove(ans + (width - len), ans, len);
-		ft_memset(ans, '0', (size_t)(width - len));
+		ans = (char *)ft_calloc((width + 1), sizeof(char));
+		ft_strlcpy(ans, p_ans, (width + 1));
+		free(p_ans);
 	}
+	else 
+		ans = p_ans;
+	if (c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X')
+		ans = padzeros(ans, width, len, '.');
 	ft_putstr_count(ans, count);
 	free(ans);
 }
